d71d47a18e92460b505258547990028e03cac700
Ilya Semyonov <mipselqq@gmail.com>
2026-01-03 23:09:36 +0300

    feat(logging): make goose use application logger via adapter

--- internal/app/startup.go
+++ internal/app/startup.go
@@ -2,7 +2,9 @@ package app
 
 import (
 	"context"
+	"fmt"
 	"log/slog"
+	"os"
 
 	"go-todo/internal/config"
 
@@ -34,7 +36,8 @@ func SetupDatabaseFromEnv(logger *slog.Logger, migrationsDir string) (*pgxpool.P
 		return nil, err
 	}
 
-	// Run migrations
+	goose.SetLogger(&gooseLogger{logger: logger})
+
 	if err := goose.SetDialect("postgres"); err != nil {
 		logger.Error("Failed to set goose dialect", slog.String("err", err.Error()))
 		return nil, err
@@ -48,4 +51,26 @@ func SetupDatabaseFromEnv(logger *slog.Logger, migrationsDir string) (*pgxpool.P
 	}
 
 	return pool, nil
+}
+
+type gooseLogger struct {
+	logger *slog.Logger
+}
+
+func (l *gooseLogger) Fatal(v ...interface{}) {
+	l.logger.Error(fmt.Sprint(v...))
+	os.Exit(1)
+}
+
+func (l *gooseLogger) Fatalf(format string, v ...interface{}) {
+	l.logger.Error(fmt.Sprintf(format, v...))
+	os.Exit(1)
+}
+
+func (l *gooseLogger) Print(v ...interface{}) {
+	l.logger.Info(fmt.Sprint(v...))
+}
+
+func (l *gooseLogger) Printf(format string, v ...interface{}) {
+	l.logger.Info(fmt.Sprintf(format, v...))
 }
