0f18d19a7924f45d55d8e39d9c9e4972e3bd784d
Ilya Semyonov <mipselqq@gmail.com>
2026-02-01 01:26:05 +0300

    refactor: factor out mocks code to separate files

--- internal/app/app.go
+++ internal/app/app.go
@@ -1,25 +1,26 @@
 package app
 
 import (
+	"log/slog"
+	"net/http"
+
 	"go-todo/internal/config"
 	"go-todo/internal/handler"
 	"go-todo/internal/repository"
 	"go-todo/internal/service"
-	"log/slog"
-	"net/http"
 
 	"github.com/jackc/pgx/v5/pgxpool"
 )
 
 func New(logger *slog.Logger, pool *pgxpool.Pool, cfg *config.AppConfig) http.Handler {
-    userRepo := repository.NewPgUser(pool)
-    authService := service.NewAuth(userRepo, service.JWTOptions{
-        JWTSecret: cfg.JWTSecret,
-        Exp:       cfg.JWTExp,
-    })
+	userRepo := repository.NewPgUser(pool)
+	authService := service.NewAuth(userRepo, service.JWTOptions{
+		JWTSecret: cfg.JWTSecret,
+		Exp:       cfg.JWTExp,
+	})
 
-    authHandler := handler.NewAuth(logger, authService)
-    healthHandler := handler.NewHealth(logger)
+	authHandler := handler.NewAuth(logger, authService)
+	healthHandler := handler.NewHealth(logger)
 
-    return NewRouter(authHandler, healthHandler)
+	return NewRouter(authHandler, healthHandler)
 }
---
+++ internal/service/mocks_test.go
@@ -0,0 +1,20 @@
+package service_test
+
+import (
+	"context"
+
+	"go-todo/internal/domain"
+)
+
+type MockUserRepository struct {
+	InsertFunc                 func(ctx context.Context, email domain.Email, hash string) error
+	GetPasswordHashByEmailFunc func(ctx context.Context, email domain.Email) (string, error)
+}
+
+func (m *MockUserRepository) Insert(ctx context.Context, email domain.Email, hash string) error {
+	return m.InsertFunc(ctx, email, hash)
+}
+
+func (m *MockUserRepository) GetPasswordHashByEmail(ctx context.Context, email domain.Email) (string, error) {
+	return m.GetPasswordHashByEmailFunc(ctx, email)
+}
