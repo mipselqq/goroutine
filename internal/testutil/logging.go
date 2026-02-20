6725935d8cd1dffef8309aedbb8be34ab674a94c
Ilya Semyonov <mipselqq@gmail.com>
2026-02-20 20:52:34 +0300

    refactor: app test

--- internal/testutil/logging.go
+++ internal/testutil/logging.go
@@ -19,3 +19,23 @@ func (w testWriter) Write(p []byte) (int, error) {
 func CreateTestLogger(t testing.TB) *slog.Logger {
 	return slog.New(slog.NewTextHandler(testWriter{t}, nil))
 }
+
+func VerifyLogValue(t *testing.T, attrs []slog.Attr, expectedAttrs map[string]string) {
+	t.Helper()
+
+	for _, a := range attrs {
+		expected, ok := expectedAttrs[a.Key]
+		if !ok {
+			t.Errorf("unexpected attribute %q", a.Key)
+			continue
+		}
+		if a.Value.String() != expected {
+			t.Errorf("for key %q, expected %q, got %q", a.Key, expected, a.Value.String())
+		}
+		delete(expectedAttrs, a.Key)
+	}
+
+	for key := range expectedAttrs {
+		t.Errorf("missing attribute %q", key)
+	}
+}
