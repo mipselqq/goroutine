acfe4a6710ed712c8fe9422e7f1d218526c6fc8b
Ilya Semyonov <mipselqq@gmail.com>
2026-02-21 15:39:31 +0300

    feat: add simple binary to check health on distroless

---
+++ cmd/ping/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
+
+func main() {
+	// This code does not reuse the config package because
+	// getEnvOrDefault is not exported and covered by tests.
+	// It doesn't worth to refactor the config package.
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	host := os.Getenv("HOST")
+	if host == "" || host == "0.0.0.0" {
+		host = "127.0.0.1"
+	}
+
+	url := fmt.Sprintf("http://%s:%s/health", host, port)
+	resp, err := http.Get(url)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "healthcheck failed: %v\n", err)
+		os.Exit(1)
+	}
+	_ = resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		fmt.Fprintf(os.Stderr, "healthcheck failed with status: %s\n", resp.Status)
+		os.Exit(1)
+	}
+
+	fmt.Println("OK")
+}
