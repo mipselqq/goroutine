8fe1bc24344f37b2fb1b6676bf22f3681e7b1489
Ilya Semyonov <mipselqq@gmail.com>
2026-02-21 16:58:52 +0300

    refactor: make main() humble for testing

--- cmd/ping/main.go
+++ cmd/ping/main.go
@@ -8,9 +8,6 @@ import (
 )
 
 func main() {
-	// This code does not reuse the config package because
-	// getEnvOrDefault is not exported and covered by tests.
-	// It isn't worth refactoring the config package.
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "8080"
@@ -21,21 +18,27 @@ func main() {
 		host = "127.0.0.1"
 	}
 
+	if err := run(host, port, 5*time.Second); err != nil {
+		fmt.Fprintf(os.Stderr, "healthcheck failed: %v\n", err)
+		os.Exit(1)
+	}
+	fmt.Println("OK")
+}
+
+func run(host, port string, timeout time.Duration) error {
 	url := fmt.Sprintf("http://%s:%s/health", host, port)
 	client := &http.Client{
-		Timeout: 5 * time.Second,
+		Timeout: timeout,
 	}
 	resp, err := client.Get(url)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "healthcheck failed: %v\n", err)
-		os.Exit(1)
+		return err
 	}
 	_ = resp.Body.Close()
 
 	if resp.StatusCode != http.StatusOK {
-		fmt.Fprintf(os.Stderr, "healthcheck failed with status: %s\n", resp.Status)
-		os.Exit(1)
+		return fmt.Errorf("healthcheck failed with status: %s", resp.Status)
 	}
 
-	fmt.Println("OK")
+	return nil
 }
