72f1505bd9226261fe249909549bc9dad12735d0
Ilya Semyonov <mipselqq@gmail.com>
2026-03-04 23:52:26 +0300

    opt(code_map): preallocate code map

--- internal/http/httpschema/code_map.go
+++ internal/http/httpschema/code_map.go
@@ -1,16 +1,16 @@
 package httpschema
 
-func MapCodeToDescription(code string) string {
-	codeMap := map[string]string{
-		"INVALID_CREDENTIALS":   "Invalid login or password",
-		"VALIDATION_ERROR":      "Some fields are invalid",
-		"INTERNAL_SERVER_ERROR": "Internal server error",
-		"NOT_FOUND":             "Resource not found",
-		"INVALID_AUTH_HEADER":   "Invalid authorization header",
-		"USER_NOT_FOUND":        "User not found",
-		"INVALID_TOKEN":         "Invalid token",
-	}
+var codeMap = map[string]string{
+	"INVALID_CREDENTIALS":   "Invalid login or password",
+	"VALIDATION_ERROR":      "Some fields are invalid",
+	"INTERNAL_SERVER_ERROR": "Internal server error",
+	"NOT_FOUND":             "Resource not found",
+	"INVALID_AUTH_HEADER":   "Invalid authorization header",
+	"USER_NOT_FOUND":        "User not found",
+	"INVALID_TOKEN":         "Invalid token",
+}
 
+func MapCodeToDescription(code string) string {
 	description, ok := codeMap[code]
 	if !ok {
 		return "Unknown error"
