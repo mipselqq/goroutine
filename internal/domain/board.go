4649d22ebbde9b322cf40db416c32d2d851d92be
Ilya Semyonov <mipselqq@gmail.com>
2026-03-07 20:28:47 +0300

    feat(domain): BoardID and Board

--- internal/domain/board.go
+++ internal/domain/board.go
@@ -1,7 +1,11 @@
 package domain
 
 import (
+	"database/sql/driver"
+	"fmt"
 	"strings"
+
+	"github.com/google/uuid"
 )
 
 const (
@@ -9,6 +13,14 @@ const (
 	ErrNameTooLong        string = "Name is too long"
 	ErrDescriptionTooLong string = "Description is too long"
 )
+
+type Board struct {
+	ID          BoardID
+	Name        Name
+	Description Description
+	CreatedAt   string
+	UpdatedAt   string
+}
 
 type Name struct {
 	value string
@@ -62,4 +74,78 @@ func (d Description) String() string {
 
 func (d Description) IsEmpty() bool {
 	return d.value == ""
+}
+
+type BoardID struct {
+	value uuid.UUID
+}
+
+func NewBoardID() BoardID {
+	id, _ := uuid.NewV7()
+	return BoardID{value: id}
+}
+
+func ParseBoardID(s string) (BoardID, error) {
+	b, err := uuid.Parse(s)
+	if err != nil {
+		return BoardID{}, fmt.Errorf("parse board id: %w", err)
+	}
+	return BoardID{value: b}, nil
+}
+
+func (b BoardID) String() string {
+	return b.value.String()
+}
+
+func (b BoardID) IsEmpty() bool {
+	return b.value == uuid.Nil
+}
+
+func (b BoardID) UUID() uuid.UUID {
+	return b.value
+}
+
+// Scan implements the sql.Scanner interface.
+func (b *BoardID) Scan(src any) error {
+	if src == nil {
+		b.value = uuid.Nil
+		return nil
+	}
+
+	switch v := src.(type) {
+	case string:
+		parsed, err := uuid.Parse(v)
+		if err != nil {
+			return err
+		}
+		b.value = parsed
+	case []byte:
+		const uuidByteLen = 16
+		isUUIDBytes := len(v) == uuidByteLen
+
+		if isUUIDBytes {
+			parsed, err := uuid.FromBytes(v)
+			if err != nil {
+				return err
+			}
+			b.value = parsed
+		} else { // Byte representation of string
+			parsed, err := uuid.ParseBytes(v)
+			if err != nil {
+				return err
+			}
+			b.value = parsed
+		}
+	default:
+		return fmt.Errorf("unexpected type for UserID: %T", src)
+	}
+	return nil
+}
+
+// Value implements the driver.Valuer interface.
+func (b BoardID) Value() (driver.Value, error) {
+	if b.IsEmpty() {
+		return nil, nil
+	}
+	return b.value.String(), nil
 }
