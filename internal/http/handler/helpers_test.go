11dbf092131baa873d732b46c4324b9c38f93c37
Ilya Semyonov <mipselqq@gmail.com>
2026-04-14 15:19:10 +0300

    fix: panic on nil mock function call instead of returning error

--- internal/http/handler/helpers_test.go
+++ internal/http/handler/helpers_test.go
@@ -2,7 +2,6 @@ package handler_test
 
 import (
 	"context"
-	"errors"
 
 	"goroutine/internal/domain"
 	"goroutine/internal/testutil"
@@ -15,14 +14,14 @@ type MockAuth struct {
 
 func (m *MockAuth) Register(ctx context.Context, email domain.Email, password domain.UserPassword) error {
 	if m.RegisterFunc == nil {
-		return errors.New("BUG: RegisterFunc is called but not set")
+		panic("BUG: Mock: AuthService.RegisterFunc is not set")
 	}
 	return m.RegisterFunc(ctx, email, password)
 }
 
 func (m *MockAuth) Login(ctx context.Context, email domain.Email, password domain.UserPassword) (string, error) {
 	if m.LoginFunc == nil {
-		return "", errors.New("BUG: LoginFunc is called but not set")
+		panic("BUG: Mock: AuthService.LoginFunc is not set")
 	}
 	return m.LoginFunc(ctx, email, password)
 }
@@ -37,35 +36,35 @@ type MockBoards struct {
 
 func (m *MockBoards) Get(ctx context.Context, ownerID domain.UserID, boardID domain.BoardID) (domain.Board, error) {
 	if m.GetFunc == nil {
-		return domain.Board{}, errors.New("BUG: GetFunc is called but not set")
+		panic("BUG: Mock: BoardsService.GetFunc is not set")
 	}
 	return m.GetFunc(ctx, ownerID, boardID)
 }
 
 func (m *MockBoards) GetMany(ctx context.Context, ownerID domain.UserID) ([]domain.Board, error) {
 	if m.GetManyFunc == nil {
-		return nil, errors.New("BUG: GetManyFunc is called but not set")
+		panic("BUG: Mock: BoardsService.GetManyFunc is not set")
 	}
 	return m.GetManyFunc(ctx, ownerID)
 }
 
 func (m *MockBoards) Create(ctx context.Context, ownerID domain.UserID, name domain.BoardName, description domain.BoardDescription) (domain.Board, error) {
 	if m.CreateFunc == nil {
-		return domain.Board{}, errors.New("BUG: CreateFunc is called but not set")
+		panic("BUG: Mock: BoardsService.CreateFunc is not set")
 	}
 	return m.CreateFunc(ctx, ownerID, name, description)
 }
 
 func (m *MockBoards) UpdateByID(ctx context.Context, ownerID domain.UserID, boardID domain.BoardID, name *domain.BoardName, description *domain.BoardDescription) (domain.Board, error) {
 	if m.UpdateByIDFunc == nil {
-		return domain.Board{}, errors.New("BUG: UpdateByIDFunc is called but not set")
+		panic("BUG: Mock: BoardsService.UpdateByIDFunc is not set")
 	}
 	return m.UpdateByIDFunc(ctx, ownerID, boardID, name, description)
 }
 
 func (m *MockBoards) Delete(ctx context.Context, ownerID domain.UserID, boardID domain.BoardID) error {
 	if m.DeleteFunc == nil {
-		return errors.New("BUG: DeleteFunc is called but not set")
+		panic("BUG: Mock: BoardsService.DeleteFunc is not set")
 	}
 	return m.DeleteFunc(ctx, ownerID, boardID)
 }
--- internal/service/helpers_test.go
+++ internal/service/helpers_test.go
@@ -2,7 +2,6 @@ package service_test
 
 import (
 	"context"
-	"errors"
 	"time"
 
 	"goroutine/internal/domain"
@@ -30,34 +29,36 @@ type MockBoardRepository struct {
 }
 
 func (m *MockBoardRepository) Create(ctx context.Context, ownerID domain.UserID, name domain.BoardName, description domain.BoardDescription) (domain.Board, error) {
-	// TODO(refactor-1): create a function assertFuncNotNil
+	if m.CreateFunc == nil {
+		panic("BUG: Mock: BoardRepository.CreateFunc is not set")
+	}
 	return m.CreateFunc(ctx, ownerID, name, description)
 }
 
 func (m *MockBoardRepository) GetByID(ctx context.Context, id domain.BoardID) (domain.Board, error) {
 	if m.GetByIDFunc == nil {
-		return domain.Board{}, errors.New("BUG: GetByIDFunc is not set")
+		panic("BUG: Mock: BoardRepository.GetByIDFunc is not set")
 	}
 	return m.GetByIDFunc(ctx, id)
 }
 
 func (m *MockBoardRepository) GetMany(ctx context.Context, ownerID domain.UserID) ([]domain.Board, error) {
 	if m.GetManyFunc == nil {
-		return nil, errors.New("BUG: GetManyFunc is not set")
+		panic("BUG: Mock: BoardRepository.GetManyFunc is not set")
 	}
 	return m.GetManyFunc(ctx, ownerID)
 }
 
 func (m *MockBoardRepository) UpdateByID(ctx context.Context, boardID domain.BoardID, name *domain.BoardName, description *domain.BoardDescription, updatedAt time.Time) (domain.Board, error) {
 	if m.UpdateByIDFunc == nil {
-		return domain.Board{}, errors.New("BUG: UpdateByIDFunc is not set")
+		panic("BUG: Mock: BoardRepository.UpdateByIDFunc is not set")
 	}
 	return m.UpdateByIDFunc(ctx, boardID, name, description, updatedAt)
 }
 
 func (m *MockBoardRepository) Delete(ctx context.Context, boardID domain.BoardID) error {
 	if m.DeleteFunc == nil {
-		return errors.New("BUG: DeleteFunc is not set")
+		panic("BUG: Mock: BoardRepository.DeleteFunc is not set")
 	}
 	return m.DeleteFunc(ctx, boardID)
 }
